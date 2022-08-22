7d04452b21325918b3c01d8dfa4b4ab3d01a791c
deepti5783 <deepti@hashtrust.in>
2022-08-22 13:37:29 +0530

    first commit

---
+++ base/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from base.models import User,Organization,jobDescription,Job_Applicant
+    
+admin.site.register(User)
+admin.site.register(Organization)
+admin.site.register(jobDescription)
+admin.site.register(Job_Applicant)
+
+
+# Register your models here.
+
---
+++ base/models.py
@@ -0,0 +1,76 @@
+from imghdr import what
+from random import choices
+from secrets import choice
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    contact_number = models.CharField(max_length=12)
+    organization = models.CharField(max_length=100, blank=True, default='') 
+
+    def _str_(self):
+        return self.first_name
+class Organization (models.Model):
+    name=models.CharField(max_length=50)
+    slug=models.SlugField(max_length=40)
+    country=models.CharField(max_length=30)
+    address=models.CharField(max_length=50)
+    contact_number=models.CharField(max_length=12)
+    email=models.EmailField()
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class jobDescription(models.Model):
+    JOB_CAT_CHOICES=(
+        ('HR','HR'),
+        ('Frontend','Frontend'),
+        ('Backend','Backend'),
+        ('Full stack','Full stack'),
+        ('Dev Ops','Dev Ops'),
+        ('BDE','BDE'),
+        ('Others','Others'),
+    )
+    
+    EMPLOYMENT_CHOICES=(
+        ('Full-time','Full-time'),
+        ('Part-time','Part-time'),
+    )
+
+    job_category=models.CharField(max_length=30,choices=JOB_CAT_CHOICES)
+    job_title=models.CharField(max_length=30)
+    job_location=models.CharField(max_length=30)
+    employment_type=models.CharField(max_length=30,choices=EMPLOYMENT_CHOICES)
+    Organization=models.CharField(max_length=40)
+    mandatory_qualification=models.CharField(max_length=40)
+    optional_qualification=models.CharField(max_length=40)
+    experience=models.IntegerField()
+    what_is_expected=models.CharField(max_length=10)
+    what_we_offer=(models.CharField(max_length=10))
+
+    def __str__(self):
+        return self.job_title
+
+    class Meta:
+        permissions=(
+            ('read_item','can read item')
+        )
+class Job_Applicant(models.Model):
+    STATUS_CHOICES=(
+        ('Pending','Pending'),
+        ('in-progress','in-progress'),
+        ('Selected','Selected'),
+        ('Rejected','Rejected'),
+    )
+
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    job_Description=models.ForeignKey(User,on_delete=models.CASCADE)
+    resume=models.FileField()
+    notice_period=models.IntegerField()
+    status=models.CharField(max_length=20,choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.user
